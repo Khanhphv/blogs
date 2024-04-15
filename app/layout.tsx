6419f14d93148aad4406bdae2dcaf148963b2a9e
khanhpham <pham.viet.khanh@tanaakk.com>
2024-04-15 09:32:02 +0700

    vercel analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Loading from "@/components/loading";
 import { SessionProvider } from "next-auth/react";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { GoogleAdsense } from "@/components/google-adsense";
+import { Analytics } from "@vercel/analytics/react";
 export default function RootLayout({
   children,
   params: { session, ...params },
@@ -27,9 +28,11 @@ export default function RootLayout({
               <div className="flex justify-between items-center"></div>
             </div>
             <div className="flex mx-auto main w-full">{children}</div>
+            <Analytics />
           </Suspense>
         </ThemeProvider>
       </body>
+
       <GoogleAdsense pId="3644774384050550" />
       <GoogleAnalytics gaId="G-SW6Y52P2PD" />
     </html>
