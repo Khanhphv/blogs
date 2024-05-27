fb2cb58f43096c2756440de40dbb03e920d662a1
Khanhphv <vietkhanh1310@gmail.com>
2024-05-27 17:13:06 +0700

    remove route and fixbug HOC

--- components/hoc/withAuth/index.tsx
+++ components/hoc/withAuth/index.tsx
@@ -4,8 +4,7 @@ import { useContext } from "react";
 
 export const withAuth = (WrappedComponent: any) => {
   // eslint-disable-next-line react/display-name
-  return (props: { post: { data: Post } }) => {
-    console.log(props);
+  return (props: any) => {
     const { isAdmin } = useContext(AuthContext);
     return (
       <WrappedComponent viewMode={!isAdmin} isAdmin={isAdmin} {...props} />
--- components/me/thumbnail/index.tsx
+++ components/me/thumbnail/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { ImgHTMLAttributes } from "react";
 
 export interface ThumbnailProps
