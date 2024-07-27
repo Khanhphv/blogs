b31c75f04308f23a8980247ae0e87db4ead371ea
Khanhphv <vietkhanh1310@gmail.com>
2024-07-27 15:26:51 +0700

    remove style

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -14,70 +14,68 @@ const KEditor = ({
   const [isLoaded, setLoaded] = useState(false);
 
   return (
-    <div style={{ display: isLoaded ? "block" : "none" }}>
-      <CKEditor
-        {...props}
-        style={{ border: 0 }}
-        editor={ClassicEditor}
-        data={data || ""}
-        onReady={(_editor: ClassicEditor) => {
-          editor.current = _editor;
-          const toolbarElement = _editor.ui.view.toolbar.element;
-          console.log("editor state:", editor.current.state);
-          if (editor.current.state === "ready") {
-            console.log(editor.current.state);
-            setLoaded(true);
-            if (isModeView && !isAdmin) {
-              if (!data) {
-                _editor.ui.view.element?.remove();
-              }
-              _editor.enableReadOnlyMode("viewMode");
-              _editor.ui.view.stickyPanel.element?.remove();
-              toolbarElement?.remove();
-              if (_editor.ui.getEditableElement()) {
-                _editor.ui.getEditableElement()!.style.border = "none";
-              }
+    <CKEditor
+      {...props}
+      style={{ border: 0 }}
+      editor={ClassicEditor}
+      data={data || ""}
+      onReady={(_editor: ClassicEditor) => {
+        editor.current = _editor;
+        const toolbarElement = _editor.ui.view.toolbar.element;
+        console.log("editor state:", editor.current.state);
+        if (editor.current.state === "ready") {
+          console.log(editor.current.state);
+          setLoaded(true);
+          if (isModeView && !isAdmin) {
+            if (!data) {
+              _editor.ui.view.element?.remove();
+            }
+            _editor.enableReadOnlyMode("viewMode");
+            _editor.ui.view.stickyPanel.element?.remove();
+            toolbarElement?.remove();
+            if (_editor.ui.getEditableElement()) {
+              _editor.ui.getEditableElement()!.style.border = "none";
             }
           }
-        }}
-        config={{
-          toolbar: [
-            "heading",
-            "|",
-            "bold",
-            "italic",
-            "link",
-            "bulletedList",
-            "numberedList",
-            "blockQuote",
+        }
+      }}
+      config={{
+        toolbar: [
+          "heading",
+          "|",
+          "bold",
+          "italic",
+          "link",
+          "bulletedList",
+          "numberedList",
+          "blockQuote",
+        ],
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
           ],
-          heading: {
-            options: [
-              {
-                model: "paragraph",
-                title: "Paragraph",
-                class: "ck-heading_paragraph",
-              },
-              {
-                model: "heading1",
-                view: "h1",
-                title: "Heading 1",
-                class: "ck-heading_heading1",
-              },
-              {
-                model: "heading2",
-                view: "h2",
-                title: "Heading 2",
-                class: "ck-heading_heading2",
-              },
-            ],
-          },
-        }}
-        onChange={() => {
-          onChange?.(editor?.current?.getData());
-        }}
-      />
-    </div>
+        },
+      }}
+      onChange={() => {
+        onChange?.(editor?.current?.getData());
+      }}
+    />
   );
 };
 
