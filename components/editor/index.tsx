6f836cd52e530a7392733200568f41be83581845
Khanhphv <vietkhanh1310@gmail.com>
2024-07-27 21:37:42 +0700

    loaded editor

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -13,69 +13,78 @@ const KEditor = ({
   const editor = useRef<any>();
   const [isLoaded, setLoaded] = useState(false);
 
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
-    <CKEditor
-      {...props}
-      style={{ border: 0 }}
-      editor={ClassicEditor}
-      data={data || ""}
-      onReady={(_editor: ClassicEditor) => {
-        editor.current = _editor;
-        const toolbarElement = _editor.ui.view.toolbar.element;
-        // console.log("editor state:", editor.current.state);
-        // if (editor.current.state === "ready") {
-        //   console.log(editor.current.state);
-        //   setLoaded(true);
-        //   if (isModeView && !isAdmin) {
-        //     if (!data) {
-        //       _editor.ui.view.element?.remove();
-        //     }
-        //     _editor.enableReadOnlyMode("viewMode");
-        //     _editor.ui.view.stickyPanel.element?.remove();
-        //     toolbarElement?.remove();
-        //     if (_editor.ui.getEditableElement()) {
-        //       _editor.ui.getEditableElement()!.style.border = "none";
-        //     }
-        //   }
-        // }
-      }}
-      config={{
-        toolbar: [
-          "heading",
-          "|",
-          "bold",
-          "italic",
-          "link",
-          "bulletedList",
-          "numberedList",
-          "blockQuote",
-        ],
-        heading: {
-          options: [
-            {
-              model: "paragraph",
-              title: "Paragraph",
-              class: "ck-heading_paragraph",
+    <>
+      {isLoaded ? (
+        <CKEditor
+          {...props}
+          style={{ border: 0 }}
+          editor={ClassicEditor}
+          data={data || ""}
+          onReady={(_editor: ClassicEditor) => {
+            editor.current = _editor;
+            const toolbarElement = _editor.ui.view.toolbar.element;
+            console.log("editor state:", editor.current.state);
+            if (editor.current.state === "ready") {
+              console.log(editor.current.state);
+              if (isModeView && !isAdmin) {
+                if (!data) {
+                  _editor.ui.view.element?.remove();
+                }
+                _editor.enableReadOnlyMode("viewMode");
+                _editor.ui.view.stickyPanel.element?.remove();
+                toolbarElement?.remove();
+                if (_editor.ui.getEditableElement()) {
+                  _editor.ui.getEditableElement()!.style.border = "none";
+                }
+              }
+            }
+          }}
+          config={{
+            toolbar: [
+              "heading",
+              "|",
+              "bold",
+              "italic",
+              "link",
+              "bulletedList",
+              "numberedList",
+              "blockQuote",
+            ],
+            heading: {
+              options: [
+                {
+                  model: "paragraph",
+                  title: "Paragraph",
+                  class: "ck-heading_paragraph",
+                },
+                {
+                  model: "heading1",
+                  view: "h1",
+                  title: "Heading 1",
+                  class: "ck-heading_heading1",
+                },
+                {
+                  model: "heading2",
+                  view: "h2",
+                  title: "Heading 2",
+                  class: "ck-heading_heading2",
+                },
+              ],
             },
-            {
-              model: "heading1",
-              view: "h1",
-              title: "Heading 1",
-              class: "ck-heading_heading1",
-            },
-            {
-              model: "heading2",
-              view: "h2",
-              title: "Heading 2",
-              class: "ck-heading_heading2",
-            },
-          ],
-        },
-      }}
-      onChange={() => {
-        onChange?.(editor?.current?.getData());
-      }}
-    />
+          }}
+          onChange={() => {
+            onChange?.(editor?.current?.getData());
+          }}
+        />
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
