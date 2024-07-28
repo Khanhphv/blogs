e8ff92de955c0163ff46ad713861af2e50388903
Khanhphv <vietkhanh1310@gmail.com>
2024-07-28 12:11:14 +0700

    dynamic metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { DetailedPostHOC } from "@/components/post/edit-post";
 import { Post } from "@/types/post";
+import type { Metadata, ResolvingMetadata } from "next";
 
 async function getData(id: string) {
   const data = await fetch(`${process.env.DOMAIN_URL}/api/post/get?id=${id}`, {
@@ -13,6 +14,21 @@ async function getData(id: string) {
     throw new Error("Failed to fetch data");
   }
   return blog;
+}
+
+export async function generateMetadata(
+  { params, searchParams }: any,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+
+  // fetch data
+  const data = (await getData(slug)) as { data: Post };
+
+  return {
+    title: data.data.title,
+  };
 }
 
 // eslint-disable-next-line @next/next/no-async-client-component
