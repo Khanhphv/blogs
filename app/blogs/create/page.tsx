734b4ada74c7da379094c3186a9b6123277c18fc
khanhpham <pham.viet.khanh@tanaakk.com>
2024-08-20 13:34:55 +0700

    style create post

--- app/blogs/create/page.tsx
+++ app/blogs/create/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   return (
-    <div className="container">
+    <div className="container" style={{ maxWidth: "80%" }}>
       <CreatePost />
     </div>
   );
