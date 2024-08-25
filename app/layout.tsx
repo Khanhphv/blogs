62f3dceb1588329602047db752e7a65a10fd0ff0
Khanhphv <vietkhanh1310@gmail.com>
2024-08-25 23:27:02 +0700

    behavior on ios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import Loading from "@/components/loading";
 import { SessionProvider } from "next-auth/react";
 import { createContext } from "react";
 import { AuthContext } from "@/components/authorize";
 import { Box, ColorSchemeProvider, ExperimentProvider } from "gestalt";
-
+import Script from "next/script";
 import "gestalt/dist/gestalt.css";
 import "gestalt-datepicker/dist/gestalt-datepicker.css";
-
+import "../public/index.js";
 export default function RootLayout({
   children,
   params: { session, ...params },
@@ -18,6 +18,44 @@ export default function RootLayout({
   children: React.ReactNode;
   params: any;
 }) {
+  const contentRef = useRef<HTMLBodyElement>(null);
+  useEffect(() => {
+    const iOS = () => {
+      return (
+        [
+          "iPad Simulator",
+          "iPhone Simulator",
+          "iPod Simulator",
+          "iPad",
+          "iPhone",
+          "iPod",
+        ].includes(navigator.platform) ||
+        // iPad on iOS 13 detection
+        (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+      );
+    };
+    if (contentRef.current && iOS()) {
+      const resizeObserver = new ResizeObserver(() => {
+        // Do what you want to do when the size of the element changes
+        const width = contentRef.current?.clientWidth || 0;
+        const height = contentRef.current?.clientHeight || 0;
+        const heightScreen = window.screen.height;
+        console.log(width, height);
+        const content = document.getElementById("khanh-content");
+        const main_content = document.getElementById("main-content");
+
+        if (width <= 640 && height > heightScreen) {
+          content?.classList.add("container-full");
+          main_content?.classList.add("wrapper");
+        } else {
+          content?.classList.remove("container-full");
+          main_content?.classList.remove("wrapper");
+        }
+      });
+      resizeObserver.observe(contentRef.current);
+      return () => resizeObserver.disconnect(); // clean up
+    }
+  }, []);
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
   return (
     <html>
@@ -26,8 +64,13 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
       </head>
-      <body suppressHydrationWarning>
+      <body ref={contentRef} suppressHydrationWarning>
         <SessionProvider>
           <ThemeProvider attribute="class" defaultTheme="dark">
             <ExperimentProvider value={{}}>
@@ -35,9 +78,20 @@ export default function RootLayout({
               {/* <Box color="default" height="100%" width="100%"> */}
               <Suspense fallback={<Loading />}>
                 <AuthContext.Provider value={{ isAdmin, setIsAdmin }}>
-                  <div className="flex flex-row max-sm:flex-col mx-auto main w-full min-h-screen ">
-                    {children}
+                  <div className="webkit-gap"></div>
+                  <div
+                    id="main-content"
+                    className=" flex flex-row max-sm:flex-col mx-auto main w-full min-h-screen "
+                  >
+                    <div className="min-h-screen" id="khanh-content">
+                      {children}
+                      {/* <div className="on-footer">
+                      </div> */}
+                    </div>
                   </div>
+                  {/* <div className="flex flex-row max-sm:flex-col mx-auto main w-full min-h-screen ">
+                    {children}
+                  </div> */}
                 </AuthContext.Provider>
               </Suspense>
               {/* </Box> */}
@@ -46,6 +100,7 @@ export default function RootLayout({
           </ThemeProvider>
         </SessionProvider>
       </body>
+      <Script src="https://example.com/script.js" />
     </html>
   );
 }
--- components/layout/main-layout.tsx
+++ components/layout/main-layout.tsx
@@ -7,12 +7,12 @@ interface Props {
 }
 export const MainLayout = (props: Props) => {
   return (
-    <div className="flex w-full h-screen max-sm:flex-col-reverse">
-      <div className="flex px-2 sm:border-r-2 gap-4 flex-col max-sm:flex-row max-sm:fixed max-sm:max-h-[50px] max-sm:w-full z-10 bg-background max-sm:border-t-2">
+    <div className="flex w-full h-max min-h-screen">
+      <div className="flex px-2 sm:border-r-2 gap-4 flex-col max-sm:flex-row max-sm:fixed max-sm:max-h-[50px] max-sm:bottom-0 max-sm:w-full z-10 bg-background max-sm:border-t-2">
         <Logo />
         <Sidebar />
       </div>
-      <div className="sm:min-h-full w-full flex overflow-auto grow max-sm:mb-[50px]">
+      <div className="sm:min-h-full w-full flex grow max-sm:mb-[50px] ">
         {props.children}
       </div>
     </div>
