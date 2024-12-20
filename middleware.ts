9dd74caaf5e71d5e279947d792fd39671883802d
Khanhphv <vietkhanh1310@gmail.com>
2024-12-20 11:07:56 +0700

    prettier

--- middleware.ts
+++ middleware.ts
@@ -8,11 +8,11 @@ export function middleware() {
   res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
   res.headers.append(
     "Access-Control-Allow-Methods",
-    "GET,DELETE,PATCH,POST,PUT"
+    "GET,DELETE,PATCH,POST,PUT",
   );
   res.headers.append(
     "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
   );
 
   return res;
