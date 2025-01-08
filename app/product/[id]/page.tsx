612decd1eb7b96d84f7576ac37b3dad875c34444
khanhpham <pham.viet.khanh@tanaakk.com>
2025-01-08 15:50:43 +0700

    remove unnecessary code

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -2,18 +2,18 @@ import { ProductDetail } from '@/components/molecules/product'
 import { IProduct } from '@/types/product'
 import { delay } from '@/utils'
 
-async function getProduct(id: string) {
-  await delay()
-  const product = await fetch('http://localhost:3000/api/products/' + id, {
-    next: { revalidate: 0 },
-  })
-  if (product.ok) return await product.json()
-  return {}
-}
+// async function getProduct(id: string) {
+//   await delay()
+//   const product = await fetch('http://localhost:3000/api/products/' + id, {
+//     next: { revalidate: 0 },
+//   })
+//   if (product.ok) return await product.json()
+//   return {}
+// }
 
 export default async function Product({ params }: { params: { id: string } }) {
   const { id } = params
-  const { data: product }: { data: IProduct } = await getProduct(id)
+  // const { data: product }: { data: IProduct } = await getProduct(id)
 
   return <>{/* <ProductDetail product={product} /> */}</>
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,13 +3,13 @@ import { IProduct } from '@/types/product'
 import { delay } from '@/utils'
 
 export default async function Products() {
-  const products = await fetch('http://localhost:3000/api/products')
-  const data = await products.json()
-  const { data: productsData } = data
+  // const products = await fetch('http://localhost:3000/api/products')
+  // const data = await products.json()
+  // const { data: productsData } = data
 
-  const productItems = productsData.map((product: IProduct) => {
-    return <ProductItem key={product.id} product={product} />
-  })
+  // const productItems = productsData.map((product: IProduct) => {
+  //   return <ProductItem key={product.id} product={product} />
+  // })
 
   return <>{/* <div className="flex flex-wrap gap-4">{productItems}</div> */}</>
 }
