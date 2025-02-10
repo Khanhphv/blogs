c03398514336b34a55244f2ea887d409dd437e12
khanhpham <pham.viet.khanh@tanaakk.com>
2025-02-10 16:31:54 +0700

    google analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { Analytics } from '@vercel/analytics/react'
 import { UserLayout } from '@/components/layout/user-layout'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
@@ -105,6 +106,7 @@ export default function RootLayout({
         </ThemeProvider>
         {/* </SessionProvider> */}
       </body>
+      <GoogleAnalytics gaId="GTM-N7TBW7X5" />
     </html>
   )
 }
